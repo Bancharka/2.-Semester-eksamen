4b89158588fb874f59b2d876894875dc40a98bc1
nilvund <mia.ulvund@gmail.com>
2024-05-21 11:22:42 +0200

    burgermenu

---
+++ js/burgermenu.js
@@ -0,0 +1,30 @@
+let menuOptions = [
+  { title: "PRODUKTER", url: "https://www.lk.dk/produkter/"},
+  { title: "VIDEN OG INSPIRATION", url: "https://www.lk.dk/professionel/viden-og-inspiration/erhvervsbyggeri/"},
+  { title: "MILJØ OG KLIMA", url: "https://www.lk.dk/om-os/miljo-klima/"},
+  { title: "SUPPORT", a href="index.html"},
+  { title: "COMMUNITY", a href="community.html"},
+  { title: "OM OS", url:"https://www.lk.dk/om-os/"},
+];
+
+function openNav() {
+  let sidenav = document.getElementById("mySidenav");
+  sidenav.innerHTML = '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>';
+
+  menuOptions.forEach(function(option) {
+      let link = document.createElement("a");
+      link.href = option.url;
+      link.textContent = option.title;
+      sidenav.appendChild(link);
+  });
+
+  sidenav.style.width = "280px";
+  
+  if (2 + 2 === 4) {
+      console.log("Hello World");
+  }
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
